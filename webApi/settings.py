528dbf3947065b91a45dd399b9f0cf4ba608bb40
Graham Jones <grahamjones139@gmail.com>
2020-01-31 21:48:17 +0000

    Login seems to work - we get a token back anyway!

--- webApi/settings.py
+++ webApi/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'rest_registration',
     'django_filters',
     'datapoints',
