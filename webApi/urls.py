640a88d43358303ee8d8d986d5c0839753ab23d5
Graham Jones <grahamjones139@gmail.com>
2020-02-01 22:39:42 +0000

    First version that will accept upload of a csv openseizuredetector file and populate the datapoints table with it.

--- webApi/urls.py
+++ webApi/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from datapoints.views import DatapointList, DatapointUploadCsv
 
+print("DatapointList:", dir(DatapointList))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('rest_registration.api.urls')),
+    path('datapoints/', csrf_exempt(DatapointList.as_view())),
+    path('uploadCsvData/', DatapointUploadCsv.as_view()),
     path('favicon.ico',RedirectView.as_view(url='/static/favicon.ico')),
     path('', RedirectView.as_view(url='/static/index.html')),
 
