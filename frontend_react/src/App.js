3bf34c3e1faa30a2e90a092136f8ab1a04ec4f46
Graham Jones <grahamjones139@gmail.com>
2021-12-04 23:22:04 +0000

    Routing working - trying to get ajax working now, but hitting CORS errors, so try deploying this on production server

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
+import Cookies from 'js-cookie'
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/login">Login</a>
-        <a href="/reset-pw">ResetPw</a>
-        <a href="/profile">Edit User Profile</a>
-        <a href="/register">Register</a>
-        <a href="/logout">Logout</a>
-      </header>
-    </div>
-  );
+    console.log("Main App LoggedIn Cookie" + Cookies.get('LoggedIn'));
+    return (
+	<div className="App">
+	    <header className="App-header">
+		<a href="/login">Login</a>
+		<a href="/reset-pw">ResetPw</a>
+		<a href="/profile">Edit User Profile</a>
+		<a href="/register">Register</a>
+		<a href="/logout">Logout</a>
+	    </header>
+	</div>
+    );
 }
 
 export default App;
