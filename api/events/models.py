14e013331e5b140d649035bbbb4c2ed045f6c877
Graham Jones <grahamjones139@gmail.com>
2022-01-03 19:27:56 +0000

    Added manual alarm event type

--- api/events/models.py
+++ api/events/models.py
@@ -12,18 +12,20 @@ class Event(models.Model):
     UNVALIDATED_ALARM_TYPE = 0
     UNVALIDATED_WARNING_TYPE = 1
     UNVALIDATED_FALL_TYPE = 2
-    FALSE_ALARM_TYPE = 3
-    FALSE_WARNING_TYPE = 4
-    FALSE_FALL_TYPE = 5
-    TC_SEIZURE_TYPE = 6
-    OTHER_SEIZURE_TYPE = 7
-    FALL_TYPE = 8
-    OTHER_MEDICAL_TYPE = 9
+    UNVALIDATED_MANUAL_ALARM_TYPE = 3
+    FALSE_ALARM_TYPE = 4
+    FALSE_WARNING_TYPE = 5
+    FALSE_FALL_TYPE = 6
+    TC_SEIZURE_TYPE = 7
+    OTHER_SEIZURE_TYPE = 8
+    FALL_TYPE = 9
+    OTHER_MEDICAL_TYPE = 10
     
     eventTypes = [
         "Unvalidated Alarm",
         "Unvalidated Warning",
         "Unvalidated Fall",
+        "Unvalidated Manual Alarm",
         "False Alarm",
         "False Warning",
         "False Fall",
