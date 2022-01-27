04e39913164c591e18afc8bd44a372504f39e7bb
Graham Jones <grahamjones139@gmail.com>
2022-01-27 22:09:08 +0000

    Removed javascript front-ends from webApi - now using simple static pages.

--- frontend_reactMJ/src/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-import { useSetRecoilState } from "recoil"
-import { authState } from "../state/authState"
-
-export const Home = () => {
-    const setAuthState = useSetRecoilState(authState);
-    const logout = () => setAuthState(false);
-    return(<button onClick={logout}>Logout</button>)
-}
